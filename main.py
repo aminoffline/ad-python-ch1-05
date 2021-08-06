6bb24c135146b5fbd350ea15c310b4dbc302bc9e
Amin <aminoffline@yahoo.com>
2021-08-07 00:45:27 +0430

    Fully Functional as needed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-
-a = "The Persian League is the largest sport event dedicated to the deprived areas of Iran. The Persian League promotes peace and friendship."\
-"This video was captured by one of our heroes who wishes peace."
+a = input()
+#a = "The Persian League is the largest sport event dedicated to the deprived areas of Iran. The Persian League promotes peace and friendship."\
+#"This video was captured by one of our heroes who wishes peace."
 a = a.split()
 b =dict(enumerate(a))
 
@@ -16,7 +16,7 @@ for i , j in b.items():
 for i in pop_keys:
     b.pop(i)
 
-
+c = {}
 for i , j in b.items():
     #print(j[0] , type(j))
     if j[0].isupper() == True:
@@ -24,32 +24,12 @@ for i , j in b.items():
             continue
         else:
             j = j.replace('.','')
-            print(f'{i+1}:{j}')
+            j = j.replace(',','')
+            c[i+1] = j
 
 
-"""
-out = []
-
-for i in a:
-    c = a.index(i)
-    b = a[c]
-    print(i , c+1  , b)
-    if b[0][0].isupper()== True:
-        if c == 0 :
-            continue
-        else:
-            i = i.replace(".", "")
-            out.append(f'{c+1}:{i}')
-    else:
-        continue
-for i in out:
-    print(i , end="\n")
-"""
-"""
-for i in b[0]:
-    
-    if b[0][0].isupper() == True :
-        print(b[0])
-    else :
-        print (None)
-"""
+if c == {}:
+    print('None')
+else:
+    for i, j in c.items():
+        print(f'{i}:{j}')
