3e739baeb7144357946c3a38d5de3e400f6b900c
Amin <aminoffline@yahoo.com>
2021-08-06 21:51:54 +0430

    input works sort of

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 a = input()
 a = a.split()
-count = {}
-#while True:
+print(a , len(a))
+out = []
+
 for i in a:
-    if i.isupper() == True:
-        c = a.index(i)
-        count[c+1] = i
+    b = a[a.index(i)]
+    c = a.index(i)
+    print(i , c+1  , b)
+    if b[0][0].isupper()== True:
+        if c == 0 :
+            continue
+        else:
+            i = i.replace(".", "")
+            out.append(f'{c+1}:{i}')
     else:
         continue
-print(count)
+for i in out:
+    print(i , end="\n")
+"""
+for i in b[0]:
+    
+    if b[0][0].isupper() == True :
+        print(b[0])
+    else :
+        print (None)
+ """
